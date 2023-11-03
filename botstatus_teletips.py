15a5bf5bcd295a702c2b2df9dde2ac3a3427bcdd
Rajesh Raki's <91279429+rajeshrakis@users.noreply.github.com>
2023-11-03 18:14:39 +0530

    botstatus_teletips.py

--- botstatus_teletips.py
+++ botstatus_teletips.py
@@ -2,7 +2,7 @@
 #You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
 # ##Credits - [BotStatus Telegram bot by TeLe TiPs] (https://github.com/teletips/Powerful_BotStatus-TeLeTiPs)
 
-# Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/teletips/Powerful_BotStatus-TeLeTiPs/blob/main/LICENSE
+# Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/rajeshrakis/Powerful_BotStatus-TeLeTiPs/blob/main/LICENSE
 
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
